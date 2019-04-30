ea889ceba5ffd8707a099cbe63fa14f1efb4fa0e
Some-pulp <34895585+Some-pulp@users.noreply.github.com>
2019-04-30 13:54:47 -0700

    Edited insert/find
    Simplified insert/find by using less counter variables. Find isn't updating iter to find the next instance of a key.

--- TextClass.cpp
+++ TextClass.cpp
@@ -1,139 +1,184 @@
+//
+// Created by Peter on 2019-04-23.
+//
+
 #include "TextClass.h"
 #include <stdexcept>
 
 
 TextClass::TextClass()
 {
-	//Constructor simplified with Double Linked List notes in Moodle
-	head = tail = iter = nullptr;
+    //Constructor simplified with Double Linked List notes in Moodle
+    head = tail = iter = nullptr;
 }
 
 void TextClass::insertHead(char val) {
-	//If list empty (head == nullptr)
-	if (!head)
-		head = tail = new Link(val);
-	else {
-		//Otherwise, make a new link w/ given value, make n point to head, tail stays nullptr
-		Link* tmp = new Link(val, head, nullptr);
-		//set p to tmp
-		head->setPrev(tmp);
-		//make head point to tmp
-		head = tmp;
-	}
-	counter++;
+    //If list empty (head == nullptr)
+    if (head == nullptr)
+        head = tail = new Link(val);
+    else {
+        //Otherwise, make a new link w/ given value, make n point to head, tail stays nullptr
+        Link* tmp = new Link(val, nullptr, head);
+        head->setPrev(tmp);
+        head = tmp;
+    }
+    counter++;
 }
 
 
 char TextClass::deleteHead() {
-	if (!head)
-		throw std::underflow_error("the list is empty");
-	
-	Link* tmp = head;
-	//Placeholder for val being removed
-	char v = tmp->getVal();
+    if (!head)
+        throw std::underflow_error("the list is empty");
 
-	//Update next for head
-	head = head->getNext();
+    Link* tmp = head;
+    //Placeholder for val being removed
+    char v = tmp->getVal();
 
-	//If the list is empty return to empty state
-	if (!head)
-		tail = nullptr;
-	else
-		//if not empty, update head to make it the first link
-		head->setPrev(nullptr);
+    //Update next for head
+    head = head->getNext();
 
-	delete tmp;
-	return v;
+    //If the list is empty return to empty state
+    if (!head)
+        tail = nullptr;
+    else
+        //if not empty, update head to make it the first link
+        head->setPrev(nullptr);
+
+    delete tmp;
+    counter--;
+    return v;
 
 }
 
 
 void TextClass::insertTail(char val) {
-	//Tail points to nullptr?
-	if (!tail)
-		tail = head = new Link(val);
-	else {
-		//Symmetric with insertHead in arguments
-		Link* tmp = new Link(val, nullptr, tail);
-		tail->setNext(tmp);
-		tail = tmp;
-	}
-	counter++;
+    //Tail points to nullptr?
+    if (tail == nullptr)
+        tail = head = new Link(val);
+    else {
+
+        Link* tmp = new Link(val, tail, nullptr);
+        tail->setNext(tmp);
+        tail = tmp;
+    }
+    counter++;
 }
 
 
 char TextClass::deleteTail() {
-	if (!tail)
-		throw std::underflow_error("the list is empty");
+    if (!tail)
+        throw std::underflow_error("the list is empty");
 
-	Link* tmp = tail;
-	char v = tmp->getVal();
-	tail = tail->getPrev();
+    Link* tmp = tail;
+    char v = tmp->getVal();
+    tail = tail->getPrev();
 
-	if (!tail)
-		head = nullptr;
-	else
-		tail->setNext(nullptr);
+    if (!tail)
+        head = nullptr;
+    else
+        tail->setNext(nullptr);
 
-	delete tmp;
-	return v;
+    delete tmp;
+    counter--;
+    return v;
 }
 
-//////////////////Iter Test/////////////////////
 bool TextClass::findKey(char key) {
-	int flag = 0;
+    Link* tmp = iter;
 
-	Link* tmp = head;
-	while (flag < counter) {
-		
-		//if the link value equals key, iter points to tmp, true
-		if (iter != nullptr)
-			tmp = iter->getNext();
-
-		if (tmp->getVal() == key) {
-			iter = tmp;
-			//delete tmp;
-			return true;
-		}
-		if (tmp->getNext() == tail)
-			tmp = head;
-		tmp = tmp->getNext();
-		flag++;
-	}
-	delete tmp;
-	return false;
-
-	//if found, iter points to selected Link, set next to next link
-	//else, iter == nullptr, return false
+    //If nothing has been found
+    if(iter == nullptr){
+        iter = head;
+        tmp = head;
+    }
+    //If the value is found
+    if(iter->getVal() == key) {
+        return true;
+    }
+    else{
+        iter = iter->getNext();
+    }
 
 
-	
+    //Until iter reaches it's starting point (tmp)
+    while(iter != tmp){
+        if(iter->getVal() == key) {
+            return true;
+        }
+
+        //if next is nullptr, wrap back to head
+        if(iter->getNext() == nullptr) {
+            iter = head;
+        }
+
+
+
+        iter = iter->getNext();
+    }
+    iter = nullptr;
+    return false;
+
 }
 
-bool TextClass::insertKey(char key) {
-	//If p is nullptr, do nothing and return
-	if (!iter->getPrev())
-		return false;
-	else {
-		Link* inserted = iter->getPrev();
-		inserted->setVal(key);
-		return true;
-	}
+
+//Three cases:
+//Middle of list
+//Head of list
+//Tail of list
+bool TextClass::insertKey(char key){
+    if(iter == nullptr)
+        return false;
+
+
+
+    //Head of list, insert
+    if(iter == head) {
+        Link* newLink = new Link(key, nullptr, head);
+        head->setPrev(newLink);
+        head = newLink;
+        return true;
+    }
+
+    //Middle of list, insert
+    //Insert at index before iter
+    Link* tmp = new Link(key, iter->getPrev(), iter);
+    iter->getPrev()->setNext(tmp);
+    iter->setPrev(tmp);
+    return true;
+
+
+
+
+    /*
+    //If p is nullptr, do nothing and return
+    if(!iter->getPrev())
+        return false;
+    else{
+        Link* inserted = iter->getPrev();
+        inserted->setVal(key);
+        return true;
+    }
+     */
 }
 
-bool TextClass::isEmpty() { return (head && tail); }
+bool TextClass::isEmpty(){return (head && tail);}
 
-bool deleteIter() {}
+bool deleteIter(){}
 
-bool deleteKey(char key) {}
+bool deleteKey(char key){}
 
-string TextClass::displayList() {
-	ostringstream list;
-	//While head not nullptr
-	while (head) {
-		//Add value to list separated by space
-		list << head->getVal();
-		head = head->getNext();
-	}
-	return list.str();
+string TextClass::displayList(){
+    ostringstream list;
+    Link* tmp = head;
+    //While head not nullptr
+    while(tmp){
+        //Add value to list separated by space
+        list << tmp->getVal();
+        //Causes head to be nullptr, not good!!!
+
+        tmp = tmp->getNext();
+    }
+
+
+    return list.str();
 }
